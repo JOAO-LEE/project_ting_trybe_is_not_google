a09a680e4558877d51d35cd0d60b0af6ec50bba8
João Lee <joaumlimaum@gmail.com>
2023-06-21 10:01:05 -0300

    Requisito 2 concluído

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+    try:
+        with open(f"{path_file}", "r") as file:
+            stacked_text = file.read().split("\n")
+            return stacked_text
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
