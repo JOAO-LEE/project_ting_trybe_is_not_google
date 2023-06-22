734dbd5addfa9413aebf695d49896b3e74040bae
João Lee <joaumlimaum@gmail.com>
2023-06-22 15:28:30 -0300

    Requisito 7 concluído

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,6 +1,5 @@
 from ting_file_management.priority_queue import PriorityQueue
 
-# from ting_file_management.file_process import process
 # import pytest
 
 
@@ -13,3 +12,11 @@ def test_basic_priority_queueing():
 
     assert queue_with_priority.dequeue() == {"qtd_linhas": 12}
     assert queue_with_priority.search(0) == {"qtd_linhas": 2}
+
+
+# def test_tries_to_remove_nothing():
+#     queue_with_priority = PriorityQueue()
+#     queue_with_priority.dequeue()
+
+#     with pytest.raises(IndexError):
+#         queue_with_priority.search(0)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,32 @@
+# from ting_file_management.file_process import process
+# from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    lower_cased_word = word.lower()
+
+    word_occurrences_dict = {"palavra": "", "arquivo": "", "ocorrencias": ""}
+    words_filtered = list()
+    counter = 0
+    encountered = list()
+    for index in range(len(instance)):
+        for j in instance.search(index)["linhas_do_arquivo"]:
+            counter += 1
+            if lower_cased_word in j.lower():
+                encountered.append({"linha": counter})
+        if encountered:
+            word_occurrences_dict["palavra"] = lower_cased_word
+            word_occurrences_dict["arquivo"] = instance.data[index][
+                "nome_do_arquivo"
+            ]
+            word_occurrences_dict["ocorrencias"] = encountered
+            words_filtered.append(word_occurrences_dict)
+    return words_filtered
+
+
+# fila = Queue()
+# process("statics/nome_pedro.txt", fila)
+# print(exists_word("Pedro", fila))
 
 
 def search_by_word(word, instance):
