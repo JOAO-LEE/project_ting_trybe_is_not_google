27b60ad1224149a1580dc319c6053f53ee1c0779
João Lee <joaumlimaum@gmail.com>
2023-06-22 16:41:18 -0300

    Requisito 8 concluído

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,5 @@
-# from ting_file_management.file_process import process
-# from ting_file_management.queue import Queue
+from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
 
 
 def exists_word(word, instance):
@@ -24,10 +24,27 @@ def exists_word(word, instance):
     return words_filtered
 
 
-# fila = Queue()
-# process("statics/nome_pedro.txt", fila)
-# print(exists_word("Pedro", fila))
-
-
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    lower_cased_word = word.lower()
+    word_occurrences_dict = {"palavra": "", "arquivo": "", "ocorrencias": ""}
+    words_filtered = list()
+    counter = 0
+    encountered = list()
+    for index in range(len(instance)):
+        for j in instance.search(index)["linhas_do_arquivo"]:
+            counter += 1
+            if lower_cased_word in j.lower():
+                encountered.append({"linha": counter, "conteudo": j})
+        if encountered:
+            word_occurrences_dict["palavra"] = lower_cased_word
+            word_occurrences_dict["arquivo"] = instance.data[index][
+                "nome_do_arquivo"
+            ]
+            word_occurrences_dict["ocorrencias"] = encountered
+            words_filtered.append(word_occurrences_dict)
+    return words_filtered
+
+
+fila = Queue()
+process("statics/nome_pedro.txt", fila)
+print(search_by_word("Pedro", fila))
